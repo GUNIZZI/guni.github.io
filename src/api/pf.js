bee135249d00e82fbdf13f0d180cd2e7b4059863
이효성 <h2dlhs@realsn.com>
2023-02-01 16:11:45 +0900

    init

--- src/api/pf.js
+++
@@ -1,9 +0,0 @@
-const API_pfList = [
-    { seq: 5, dt: "제목 5", at: "내용5", date: "2021-02-04" },
-    { seq: 4, dt: "제목 4", at: "내용4", date: "2021-02-04" },
-    { seq: 3, dt: "제목 3", at: "내용3", date: "2021-02-04" },
-    { seq: 2, dt: "제목 2", at: "내용2", date: "2021-02-04" },
-    { seq: 1, dt: "제목 1", at: "내용1", date: "2021-02-04" },
-];
-
-export default API_pfList;
